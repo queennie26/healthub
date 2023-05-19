99b2e5e9293b8dd5ab9b1ffba6765e3c0e91f0b8
queennie26 <queenniearo.265@gmail.com>
2023-05-20 01:03:01 +0800

    log in and reg

--- app/src/main/java/com/example/healthub/LoginActivity.java
+++ app/src/main/java/com/example/healthub/LoginActivity.java
@@ -2,13 +2,51 @@ package com.example.healthub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText edUsername, edPassword;
+    Button btn;
+    TextView tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edUsername = findViewById(R.id.editTextUsername);
+        edPassword = findViewById(R.id.editTextPassword);
+        btn = findViewById(R.id.buttonLog);
+        tv = findViewById(R.id.textViewRegNew);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = edUsername.getText().toString();
+                String password = edPassword.getText().toString();
+                if(username.length()==0 || password.length()==0){
+                    Toast.makeText(getApplicationContext(), "Please fill all Details", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+
     }
 }
